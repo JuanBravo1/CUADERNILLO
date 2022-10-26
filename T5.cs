f08cc9a44744066252cfefbc3adf4e4d39050847
Wr3nch^-^ <116601168+JuanBravo1@users.noreply.github.com>
2022-10-26 00:26:53 -0500

    Add files via upload

---
+++ T5.cs
@@ -0,0 +1,33 @@
+﻿namespace a
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double FINAL, PRECIO, CANT;
+            Console.WriteLine("Introduce el precio:  ");
+            PRECIO = double.Parse(Console.ReadLine());
+            Console.WriteLine("Introduce la cantidad: ");
+            CANT = double.Parse(Console.ReadLine());
+
+            if (CANT >= 2)
+            {
+                FINAL = (PRECIO * CANT) + ((PRECIO * CANT) + ((PRECIO * CANT) * 0.05));
+                Console.WriteLine("El precio más descuento es:  "+FINAL);
+                Console.ReadKey();
+            }
+            else if (CANT <= 1)
+            {
+                FINAL = PRECIO * CANT;
+                Console.WriteLine("El precio más descuento es:  " + FINAL);
+                Console.ReadKey();
+            }
+            else
+            {
+                Console.WriteLine("error");
+            }
+
+
+        }
+    }
+}
